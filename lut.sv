96298650fb06a5204cd953744c25b5c3be20adc0
Gurjan-Singh-Samra <gurjan@LAPTOP-5PIKICUO>
2024-06-05 11:48:09 +0100

    added multiple read ports for lut

--- lut.sv
+++ lut.sv
@@ -1,11 +1,12 @@
 module lut #(
     parameters  DATA_WIDTH = 32,
                 RBG_SIZE = 24,
-                MAX_ITERATION
-) (
-    input logic [DATA_WIDTH-1:0]    iterations,
+                MAX_ITERATION = 50,
+                NUM_ENGINES = 4
+)(
+    input logic [DATA_WIDTH-1:0]    iterations [NUM_ENGINES-1:0],
     
-    output logic [RBG_SIZE-1:0]     rbg_val
+    output logic [RBG_SIZE-1:0]     rbg_val [NUM_ENGINES-1:0]
 );
     
 logic [DATA_WIDTH-1:0] lut_array [MAX_ITERATION-1:0];
@@ -16,7 +17,9 @@ initial begin
     $display("rom Loaded");
 end
 always_comb begin
-    rbg_val = lut_array[iterations];
+    for(int i = 0; i < NUM_ENGINES-1; i++)begin
+        rbg_val[i] = lut_array[iterations[i]];
+    end
 end
 
 endmodule
