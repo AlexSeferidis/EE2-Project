40e7b7d8e3f8b74ec5e6268d98ebc7d1bb9019ae
AlexSeferidis <alex.seferidis@gmail.com>
2024-06-05 15:11:53 +0100

    finished FPGA client code structure

--- FPGA_Client/poll_test.py
+++ FPGA_Client/poll_test.py
@@ -3,26 +3,30 @@ import json # not sure if this is needed
 import websockets
 import requests
 import urllib3
-from time import sleep
+import time
 
 # urllib3.disable_warnings() 
 
 # SERVER SIDE 
-URL = "http://ec2-18-130-114-167.eu-west-2.compute.amazonaws.com:8080/"
+URL = "http://ec2-18-130-114-167.eu-west-2.compute.amazonaws.com:8080/parameters/"
 POLL_INTERVAL = 5 # Interval for polling of server
 
 
-
-# jsonbody = {"x_offset": x_offset, "y_offset": y_offset, "zoom": zoom, "max_iterations": max_iterations}
+        
+    # jsonbody = {"x_offset": x_offset, "y_offset": y_offset, "zoom": zoom, "max_iterations": max_iterations}
+    
 while True:
+    try:
         response = requests.get(URL, verify=False)
         if response.status_code == 200: # if request succeded
-            rx_data = response.json()
+            data = response.json()
             x, y, zoom, maxitr = data['x_offset'], data['y_offset'], data['zoom'], data['max_iterations']
+            print("Received data from server:")
             print("x = " + str(x))
             print("y = " + str(y))
             print("zoom = " + str(zoom))
             print("maxitr = " + str(maxitr))
+            print("\n")
         else: # in case of failed request
             print(f"HTTP Request Failed")
             print(f"Status code: {response.status_code}")
