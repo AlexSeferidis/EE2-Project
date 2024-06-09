2043259d30f355e06030b90fc11881e3cff3960e
Cheesypasta1 <trivedi.dhyey@yahoo.co.uk>
2024-06-09 16:19:14 +0100

    spelling

---
+++ single_engine_rewrite.sv
@@ -0,0 +1,20 @@
+module single_engine (
+    input logic clk,
+    input logic reset,
+    input logic [31:0] iterations_max,
+    input logic signed [fp_bits - 1:0] x0,
+    input logic signed [fp_bits - 1:0] y0,
+
+    output logic finished,
+    output logic [31:0] iterations
+  );
+
+
+
+
+
+
+
+
+
+endmodule
