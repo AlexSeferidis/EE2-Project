3bf04a4a86e0534ad57bcf8f86ff54206189b179
Gurjan-Singh-Samra <gurjan@LAPTOP-5PIKICUO>
2024-06-11 16:34:12 +0100

    not working

--- obj_dir/Vtop__Syms.h
+++ obj_dir/Vtop__Syms.h
@@ -4,34 +4,36 @@
 // Internal details; most calling programs do not need this header,
 // unless using verilator public meta comments.
 
-#ifndef _VTOP__SYMS_H_
-#define _VTOP__SYMS_H_  // guard
+#ifndef VERILATED_VTOP__SYMS_H_
+#define VERILATED_VTOP__SYMS_H_  // guard
 
-#include "verilated_heavy.h"
+#include "verilated.h"
 
-// INCLUDE MODULE CLASSES
+// INCLUDE MODEL CLASS
+
 #include "Vtop.h"
 
-// SYMS CLASS
-class Vtop__Syms : public VerilatedSyms {
+// INCLUDE MODULE CLASSES
+#include "Vtop___024root.h"
+
+// SYMS CLASS (contains all model state)
+class Vtop__Syms final : public VerilatedSyms {
   public:
-    
-    // LOCAL STATE
-    const char* __Vm_namep;
-    bool __Vm_activity;  ///< Used by trace routines to determine change occurred
-    uint32_t __Vm_baseCode;  ///< Used by trace routines when tracing multiple models
-    bool __Vm_didInit;
-    
-    // SUBCELL STATE
-    Vtop*                          TOPp;
-    
-    // CREATORS
-    Vtop__Syms(Vtop* topp, const char* namep);
-    ~Vtop__Syms() {}
-    
+    // INTERNAL STATE
+    Vtop* const __Vm_modelp;
+    bool __Vm_activity = false;  ///< Used by trace routines to determine change occurred
+    uint32_t __Vm_baseCode = 0;  ///< Used by trace routines when tracing multiple models
+    bool __Vm_didInit = false;
+
+    // MODULE INSTANCE STATE
+    Vtop___024root                 TOP;
+
+    // CONSTRUCTORS
+    Vtop__Syms(VerilatedContext* contextp, const char* namep, Vtop* modelp);
+    ~Vtop__Syms();
+
     // METHODS
-    inline const char* name() { return __Vm_namep; }
-    
+    const char* name() { return TOP.name(); }
 } VL_ATTR_ALIGNED(VL_CACHE_LINE_BYTES);
 
 #endif  // guard
